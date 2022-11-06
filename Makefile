62d0cc1b6ffaf3762f9e3f5733ed2b984f2b7a64
tesscat <onetesseract@gmail.com>
2022-11-06 20:17:36 +0000

    bootloop fixed, gcc be gone

--- Makefile
+++ Makefile
@@ -10,17 +10,17 @@ ARCH_DIR := $(SRC_DIR)/$(TARGET)/arch/$(ARCH)/
 
 PREFIX := $(ARCH)-elf
 
-AS := $(PREFIX)-as
-LD := $(PREFIX)-ld
+AS := clang
+LD := clang
 SILVERC := x
 SILVERC_2 := clang
-SILVERC_2_FLAGS := -ffreestanding -Wall -Werror -Wextra -fno-exceptions -fno-rtti
+SILVERC_2_FLAGS := -ffreestanding -Wall -Wextra -fno-exceptions -fno-rtti
 
-AS_FLAGS :=
-FLAGS := -ffreestanding -nostdlib -mno-red-zone -mno-sse -mno-sse2 -mno-mmx -mno-avx -shared
+FLAGS := -ffreestanding -nostdlib -mno-red-zone -mno-sse -mno-sse2 -mno-mmx -mno-avx
+AS_FLAGS := $(FLAGS)
 
-AS_FLAGS_32 := --32
-FLAGS_32 := -m elf_i386
+AS_FLAGS_32 := -target i686-elf
+FLAGS_32 := -target i686-elf
 
 default: $(OUT_DIR)/$(TARGET)
 
@@ -31,23 +31,24 @@ include $(ARCH_DIR)/make.config
 
 $(BUILD_DIR)/%.32.o: $(SRC_DIR)/%.32.silver
 	mkdir -p $(shell dirname $@)
-	$(SILVERC) $< &> /dev/null
+	$(SILVERC) $< 1> /dev/null
 	mv $(SRC_DIR)/$(*).32.ll $(BUILD_DIR)/$(*).32.ll
-	$(SILVERC_2) -S -m32 -no-integrated-as $(BUILD_DIR)/$(*).32.ll -o $(BUILD_DIR)/$(*).32.S
-	$(AS) $(AS_FLAGS) $(AS_FLAGS_32) $(BUILD_DIR)/$(*).32.S -o $@
+	$(SILVERC_2) -c $(SILVERC_2_FLAGS) $(FLAGS_32) $(BUILD_DIR)/$(*).32.ll -o $@
+	# $(SILVERC_2) -S -m32 -no-integrated-as $(BUILD_DIR)/$(*).32.ll -o $(BUILD_DIR)/$(*).32.S
+	# $(AS) $(AS_FLAGS) $(AS_FLAGS_32) $(BUILD_DIR)/$(*).32.S -o $@
 	# $(SILVERC_2) $(SILVERC_2_FLAGS) -target i386-unknown-none-elf -c $(BUILD_DIR)/$(*).32.S -o $@
 
 $(BUILD_DIR)/%.32.o: $(SRC_DIR)/%.32.s
 	mkdir -p $(shell dirname $@)
-	$(AS) $(AS_FLAGS) $(AS_FLAGS_32) $< -o $@
+	$(AS) $(AS_FLAGS) $(AS_FLAGS_32) -c $< -o $@
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.silver
 	mkdir -p $(shell dirname $@)
-	$(SILVERC) $< &> /dev/null
+	$(SILVERC) $< 1> /dev/null
 	mv $(SRC_DIR)/$(*).ll $(BUILD_DIR)/$(*).ll
 	$(SILVERC_2) -S $(BUILD_DIR)/$(*).ll -o $(BUILD_DIR)/$(*).S
 	$(SILVERC_2) $(SILVERC_2_FLAGS) -c $(BUILD_DIR)/$(*).S -o $@
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.s
 	mkdir -p $(shell dirname $@)
-	$(AS) $(AS_FLAGS) $< -o $@
+	$(AS) $(AS_FLAGS) -c $< -o $@
--- src/kernel/arch/x86_64/src/boot/boot.32.s
+++ src/kernel/arch/x86_64/src/boot/boot.32.s
@@ -1,53 +1,53 @@
-/ * Multiboot header constants */
-.set ALIGN, 1<<0 /* align loaded modules on page boundaries */
-.set MEMINFO, 1<<1 /* provide memory map */
-.set FLAGS, ALIGN | MEMINFO /* flags */
-.set MAGIC, 0x1BADB002 /* ew magic number */
-.set CHECKSUM, -(MAGIC + FLAGS) /* checksum for validation */
+; /* Multiboot header constants */
+.set ALIGN, 1<<0 ; /* align loaded modules on page boundaries */
+.set MEMINFO, 1<<1 ; /* provide memory map */
+.set FLAGS, ALIGN | MEMINFO ; /* flags */
+.set MAGIC, 0x1BADB002 ; /* ew magic number */
+.set CHECKSUM, -(MAGIC + FLAGS) ; /* checksum for validation */
 
-/* Declare the multiboot section. This marks the program as a mb-compliant kernel.
-   These are "magic" values from the MB spec. This will be searched for in the first
-   8 KiB of the file, at 32-bit alignment.
-   We make a separate section to force alignment. */
+; /* Declare the multiboot section. This marks the program as a mb-compliant kernel.
+   ; These are "magic" values from the MB spec. This will be searched for in the first
+   ; 8 KiB of the file, at 32-bit alignment.
+   ; We make a separate section to force alignment. */
 .section .multiboot
-.align 4 /* align to the 32-bits we mentioned earlier */
-.long MAGIC /* just write the data out */
+.align 4 ; /* align to the 32-bits we mentioned earlier */
+.long MAGIC ; /* just write the data out */
 .long FLAGS
 .long CHECKSUM
 
-/* set-up the stack, in an unititialised data (bss) section. */
+; /* set-up the stack, in an unititialised data (bss) section. */
 .section .bss
-.align 16 /* Demanded by System V ABI Standard, needed for compiler good behaviour */
+.align 16 ; /* Demanded by System V ABI Standard, needed for compiler good behaviour */
 stack_bottom:
-.skip 16384 /* 16 KiB */
+.skip 16384 ; /* 16 KiB */
 stack_top:
 
-/* woo the start bit */
+; /* woo the start bit */
 .section .text
 .global _start
 .type _start, @function
 _start:
-  /* Bootloader gives us:
-    - 32-bit protected mode
-    - Interrupts and paging disabled
-    - Multiboot-compliant environment
-  */
+  ; /* Bootloader gives us:
+  ;  - 32-bit protected mode
+  ;  - Interrupts and paging disabled
+  ;  - Multiboot-compliant environment
+  ; */
 
-  /* make a stack */
+  ; /* make a stack */
   mov $stack_top, %esp
 
-  /*
-  TODO: GDT and Paging before we jump into fancy code
-  */
+  ; /*
+  ; TODO: GDT and Paging before we jump into fancy code
+  ; */
 
-  ; call kernel_start
+  call kernel_start
 
-  /* the kernel_start has returned */
-  /* clear/disable interrupts, halt, and jump to halt if we escape. */
+  ; /* the kernel_start has returned */
+  ; /* clear/disable interrupts, halt, and jump to halt if we escape. */
 
   cli
 1:hlt
   jmp 1b
 
-/* call tracing things */
+; /* call tracing things */
 .size _start, . - _start
