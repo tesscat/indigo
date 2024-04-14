41e5e1f08784e00a628aa9b8f2405d458cc46c67
tesscat <tesscat.cat@gmail.com>
2024-04-14 22:15:24 +0100

    newline at end of panic to ensure flush

--- src/kernel/arch/x86_64/src/util/util.cpp
+++ src/kernel/arch/x86_64/src/util/util.cpp
@@ -4,7 +4,7 @@
 namespace util {
 
 [[noreturn]] void _panic(const char *msg, const char *file, const char *line, const char *func) noexcept {
-    logs::info << "\nPANIC! at " << file << ":" << line << " (`" << func << "`): " << msg;
+    logs::info << "\nPANIC! at " << file << ":" << line << " (`" << func << "`): " << msg << '\n';
     loop_forever;
 }
 
